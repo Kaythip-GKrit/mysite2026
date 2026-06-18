5e507f3d39205d536551e4227a6ac0310c2b94b9
kaythip-krit <kaythipgkrit@gmail.com>
2026-06-18 16:33:47 +0700

    feat: initialize Django project with custom settings, routing, and home template

--- mysite2026/urls.py
+++ mysite2026/urls.py
@@ -2,7 +2,7 @@
 from django.contrib import admin
 from django.urls import path,include
 from django.http import HttpResponse
-
+from django.shortcuts import render
 def info(request):
     ip_adress = request.META['REMOTE_ADDR']
     res_text = f"Your IP Address is :{ip_adress}\n"
@@ -10,9 +10,10 @@ def info(request):
     for k, v in request.headers.items():
         res_text += f"<p>{k} :{v}</p>"
     return HttpResponse(res_text)
-
+def home(request):
+    return render(request,'home.html',)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('info/', info),
-    path('', include('blog.urls')),
+    path('', home),
 ]
